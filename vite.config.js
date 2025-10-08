c4927e77dc22de43af5beb0d31883c84b6e1f5b0
Forammodi3103 <forammodi3103@gmail.com>
2025-10-08 14:27:52 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/tic_tac_toe/', // ✅ fixes 404 errors on GitHub Pages
+  base: '/tic_tac_toe/',
 })
